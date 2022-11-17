a41a456ce203e1b76d51975c21cc3c9b24355443
angel <angel@DESKTOP-TMJ6D80>
2022-11-17 16:57:13 -0600

    Agregar archivos de proyecto.

---
+++ InterfazManoRobotica/InterfazManoRobotica.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace InterfazManoRobotica
+{
+    partial class InterfazManoRobotica
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // InterfazManoRobotica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 272);
+            this.KeyPreview = true;
+            this.Name = "InterfazManoRobotica";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InterfazManoRobotica_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ InterfazManoRobotica/InterfazManoRobotica.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Windows.Forms;
+
+namespace InterfazManoRobotica
+{
+    public partial class InterfazManoRobotica : Form
+    {
+        int count = 0;
+
+        public InterfazManoRobotica() => InitializeComponent();
+
+        private void InterfazManoRobotica_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.Text = ""+count++;
+        }
+    }
+}
